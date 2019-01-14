49adfe6fa7edf66bc7484ea9a84eca8d21884ccc
barkhyehyeon <barkhyehyeon@gmail.com>
2019-01-14 11:39:55 +0900

    modify overall structure; fix bugs for search

--- projects/project1/project1/application.py
+++ projects/project1/project1/application.py
@@ -22,7 +22,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
@@ -31,16 +31,17 @@ def login():
 @app.route("/logout")
 def logout():
 	session["userid"] = None
+	session["isbn"] = None
 	return render_template("login.html", message="Logged out")
 
 @app.route("/register")
 def register():
-	return render_template("register.html", message="Register wigh username and password")
+	return render_template("register.html", message="Register with username and password")
 
 @app.route("/registered", methods=["POST"])
 def registered():
 	if request.method == "GET":
-		render_template("register.html", message="Register unsuccessful")
+		render_template("register.html", message="Invalid access")
 	username = request.form.get("username")
 	password = request.form.get("password")
 	existingUser = db.execute("SELECT * FROM users WHERE username = :username", {"username":username})
@@ -54,29 +55,37 @@ def registered():
 def search():
 	if request.method == "GET":
 		if session.get("userid") is None:
-			return render_template("login.html", message="Please log in first")
+			return render_template("login.html", message="Invalid access")
 	if request.method == "POST":
 		username = request.form.get("username")
 		password = request.form.get("password")
-		row = db.execute("SELECT * FROM users WHERE username = :username and password = :password", {"username":username, "password":password})
-		if row.rowcount != 1:
+		user = db.execute("SELECT * FROM users WHERE username = :username and password = :password", {"username":username, "password":password})
+		if user.rowcount != 1:
 			return render_template("login.html", message="Wrong username or password")
-		user = row.fetchone()
-		session["userid"] = user["userid"]
+		user = user.fetchone()
+		session["userid"] = user['userid']
 	db.commit()
 	return render_template("search.html")
 
 @app.route("/result", methods=["POST"])
 def result():
 	if request.method == "GET":
-		return "Invalid access"
-	keyword = '%' + request.form.get("keyword") + '%'
-	result = db.execute("SELECT * FROM books WHERE isbn LIKE :keyword or title LIKE :keyword or author LIKE :keyword", {"keyword":keyword})
+		if session.get("userid") is None:
+			return render_template("login.html", message="Invalid access")
+		else:
+			return render_template("search.html", message="Invalid access")
+	keyword = '%' + request.form.get("keyword").lower() + '%'
+	result = db.execute("SELECT * FROM books WHERE LOWER(isbn) LIKE :keyword or LOWER(title) LIKE :keyword or LOWER(author) LIKE :keyword", {"keyword":keyword})
 	db.commit()
 	return render_template("result.html", result=result)
 
 @app.route("/bookpage", methods=["POST"])
 def bookpage():
+	if request.method == "GET":
+		if session.get("userid") is None:
+			return render_template("login.html", message="Invalid access")
+		else:
+			return render_template("search.html", message="Invalid access")
 	isbn = request.form.get("isbn")
 	session["isbn"] = isbn
 	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn":isbn}).fetchone()
@@ -97,6 +106,11 @@ def bookpage():
 
 @app.route("/reviewed", methods=["POST"])
 def reviewed():
+	if request.method == "GET":
+		if session.get("userid") is None:
+			return render_template("login.html", message="Invalid access")
+		else:
+			return render_template("search.html", message="Invalid access")
 	isbn = session.get("isbn")
 	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn":isbn}).fetchone()
 
@@ -111,31 +125,27 @@ def reviewed():
 
 	reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn", {"isbn":isbn}).fetchall()
 
+	###
 	userid = session.get("userid")
 	rating = float(request.form.get("rating"))
 	description = request.form.get("description")
-
 	review = db.execute("SELECT * FROM reviews where isbn = :isbn and userid = :userid", {"isbn":isbn, "userid":userid})
 	if review.rowcount != 0:
-		return render_template("bookpage.html", book=book, count=count, average=average, reviews=reviews, message="Review for this book already exists")
-
+		return render_template("bookpage.html", book=book, count=count, average=average, reviews=reviews, message="This book is already reviewed")
 	db.execute("INSERT INTO reviews (isbn, userid, rating, description) VALUES (:isbn, :userid, :rating, :description)", 
-		{"isbn":isbn, "userid":userid, "rating":rating, "description":description})
-
-	
-	reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn", {"isbn":isbn}).fetchall()
-	
+		{"isbn":isbn, "userid":userid, "rating":rating, "description":description})	
+	reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn", {"isbn":isbn}).fetchall()	
 	db.commit()
-	return render_template("bookpage.html", book=book, count=count, average=average, reviews=reviews, message="Review submission successful")
+	return render_template("bookpage.html", book=book, count=count, average=average, reviews=reviews, message="Review submitted")
 
 @app.route("/api/<string:isbn>")
 def getJson(isbn):
 	book = db.execute("SELECT * FROM books where isbn = :isbn", {"isbn":isbn}).fetchone()
-	dict = {}
-	dict["title"] = book["title"]
-	dict["author"] = book["author"]
-	dict["year"] = book["year"]
-	dict["isbn"] = book["isbn"]
+	tmp = dict()
+	tmp["title"] = book["title"]
+	tmp["author"] = book["author"]
+	tmp["year"] = book["year"]
+	tmp["isbn"] = book["isbn"]
 
 	res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "b0gIucYZHfS9iMHGQkqDQ", "isbns": isbn})
 	try:
@@ -146,6 +156,7 @@ def getJson(isbn):
 		count = "NA"
 		average = "NA"
 
-	dict["ratings_count"] = count
-	dict["average_score"] = average
+	tmp["ratings_count"] = count
+	tmp["average_score"] = average
+
 	return json.dumps(dict)
